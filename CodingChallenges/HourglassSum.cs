8072f94446e1eefb7cefcad234ea76323a1b36cd
Nigel McIntyre <nigelmcintyre1995@gmail.com>
2020-03-12 14:15:54 +0000

    Add project files.

---
+++ CodingChallenges/HourglassSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingChallenges
+{
+    class HourglassSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
